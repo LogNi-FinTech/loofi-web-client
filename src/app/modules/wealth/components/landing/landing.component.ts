b74ae113935f2b5cff3461eabf74297a26b3fbdc
loofidev001 <loofidev001@gmail.com>
2024-01-01 22:35:34 +0600

    Implemented credit and wealth module

--- src/app/modules/wealth/components/landing/landing.component.ts
+++ src/app/modules/wealth/components/landing/landing.component.ts
@@ -1,9 +1,7 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { HomeService } from 'app/modules/home/services/home.service';
-import { UserService } from 'app/shared/service/user.service';
-import { take } from 'rxjs/operators';
+import { WealthService } from '../../Service/wealth.service';
 
 @Component({
   selector: 'app-landing',
@@ -23,70 +21,19 @@ export class LandingComponent implements OnInit {
     }
   ];
   public isXsStyleActive: boolean = false;
-  filterTransaction : FormGroup;
-  public transactions: any[];
-  public storedTransactions: any[];
-  private identifier: string;
+  public wealths: any[];
   public accountInfo: any;
   public balanceInfo: any;
   public isLoading: boolean = true;
+  filterTransaction : FormGroup;
 
   constructor(private breakpointObserver: BreakpointObserver,
-    private homeService: HomeService,
-    private userService: UserService) { }
+    private wealthService : WealthService) { }
 
   ngOnInit(): void {
-    this.identifier =  this.userService.getIdentifier();
     this.initializeForm();
     this.subscribeToBreakPoint();
-    this.getTransactions();
-    setTimeout(() => {
-      this.filterTransaction.get("filterTransactionControl").valueChanges.subscribe((data) => {
-        console.log('Selected Value:', data);
-        if(data.value == 1){
-          this.transactions = this.storedTransactions; 
-          return;
-        }
-        else if(data.value == 2){
-          this.transactions = this.storedTransactions.filter(item => !item.MoneyDirection); 
-          return;
-        }
-        this.transactions = this.storedTransactions.filter(item => item.MoneyDirection); 
-      });
-    });
-  }
-
-  // getAccountInfo(){
-  //   this.homeService.getAccountInfo(this.identifier).pipe(take(1)).subscribe(accountInfo => {
-  //     this.accountInfo = accountInfo;
-  //     this.getBalanceInfo();
-  //   })
-  // }
-
-  // getBalanceInfo(){
-  //   this.homeService.getBalanceInfo(this.identifier).pipe(take(1)).subscribe(balanceInfo => {
-  //     this.balanceInfo = balanceInfo;
-  //     this.getTransactions();
-  //   });
-  // }
-  
-  private getTransactions(){
-    this.homeService.getTransactions(this.identifier).pipe(take(1)).subscribe(transactions => {
-      this.transactions = this.formatTransactions(transactions.content)
-      this.isLoading = false;
-      this.storedTransactions = this.transactions;
-    });
-    //this.transactions = this.formatTransactionDates(this.homeService.getTransActionData());
-  }
-
-  private formatTransactions(transactions: any[]): any[] {
-    return transactions.map((transaction) => ({
-      ...transaction,
-      image: '1.jpg',
-      MoneyDirection: transaction.amount > 0 ? true : false,
-      amount: transaction.amount < 0 ? transaction.amount * -1 : transaction.amount, 
-      formattedDate: transaction.txnTime,
-    }));
+    this.wealths = this.wealthService.gerWealths();
   }
 
   initializeForm(){
